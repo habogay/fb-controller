3226bf735f65f7ce0cd312f92a9c42621e258002
Ha Bogay <habogay@gmail.com>
2020-09-29 17:39:32 +0700

    facebook controller

--- src/main/java/com/fcs/controller/HomeController.java
+++ src/main/java/com/fcs/controller/HomeController.java
@@ -1,38 +1,43 @@
 package com.fcs.controller;
 
+import java.io.File;
 import java.util.concurrent.ExecutionException;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.google.cloud.firestore.Firestore;
-import com.google.cloud.firestore.FirestoreOptions;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.Tesseract;
+
+
 
 @Controller
 
 public class HomeController {
 
-	@Autowired
-	Firestore firestore;
-
 	@GetMapping(value = "/")
-	public String index() throws InterruptedException, ExecutionException {
-
+	public String index(Model model) throws InterruptedException, ExecutionException {
+		String rs="";
+		ITesseract tess = new Tesseract();
+		  try {
+		    // Specify trained data folder
+		    // tess.setDatapath("./tessdata");
+		    // Specify detected language 
+		    tess.setLanguage("eng");
+		    File img = new File("/home/habogay/Desktop/lh.png");
+		    rs = tess.doOCR(img);
+		    model.addAttribute("rs", rs);
+		    System.out.println(rs);
+		  } catch (Exception e) {
+		    System.out.println(e.getMessage());
+		  }
 		return "views/home/index";
 	}
+	
 	@GetMapping(value = "/welcome")
 	public String welcome() throws InterruptedException, ExecutionException {
 
 		return "welcome";
-	}
-	@GetMapping(value = "/firestore/libversion")
-	@ResponseBody
-	public String status() throws InterruptedException, ExecutionException {
-		
-		return firestore.getOptions().getLibraryVersion();
 	}
 
 }
